de77bd61fdf9d26c716d0d1f8c6f7774fd9c5398
VitaliyJV <114652130+VitaliyJV@users.noreply.github.com>
2023-06-10 09:44:47 +0300

    1

---
+++ src/main/java/org/example/Convert.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import com.google.gson.Gson;
+
+public class Convert {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is Gradle project.");
+        MyPerson p1 = new MyPerson("Vitaliy", "Volokitkin");
+        Gson gson = new Gson();
+        String maxP1 = gson.toJson(p1);
+        System.out.println(maxP1);
+    }
+
+}
